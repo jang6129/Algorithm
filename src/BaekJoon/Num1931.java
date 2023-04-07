ac1b0c103c844d1936a2bc7fde78c99dc546791a
Doma-bitcamp <jang6129@naver.com>
2023-04-07 18:10:02 +0900

    BaekJon 1931 solve

--- src/BaekJoon/Num1931.java
+++ src/BaekJoon/Num1931.java
@@ -1,68 +1,44 @@
-//package BaekJoon;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.Arrays;
-//import java.util.Comparator;
-//import java.util.LinkedList;
-//import java.util.PriorityQueue;
-//import java.util.Queue;
-//import java.util.TreeMap;
-//
-//public class Num1931 {
-//	public static void main(String[] args) {
-//		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-//		int n = 0;
-//		int counter = 0;
-//		long currentTime = 0;
-//		PriorityQueue<long[][]> queue = new PriorityQueue<long[][]>(new Comparator<long[][]>();
-//
-//		try {
-//			n = Integer.parseInt(bf.readLine());
-//			for (int i = 0; i < n; i++) {
-//				String[] input = bf.readLine().split(" ");
-//				long[][] inputToLong = new long[Integer.parseInt(input[1])][Integer.parseInt(input[0])];
-//				queue.add(inputToLong);
-//			}
-//		} catch (NumberFormatException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		while (!map.isEmpty()) {
-//			System.out.println(map.pollFirstEntry().getKey());
-//		}
-//
-//		if (!map.isEmpty()) {
-//			currentTime = map.pollFirstEntry().getKey();
-//			counter++;
-//		}
-//
-//		while (!map.isEmpty()) {
-//			if (map.firstEntry().getValue() >= currentTime) {
-//				currentTime = map.pollFirstEntry().getKey();
-//				counter++;
-//			} else {
-//				map.pollFirstEntry();
-//			}
-//		}
-//
-//		System.out.println(counter);
-//
-//	}
-//
-//	Arrays.sort(queue, new Comparator<long[]>() {
-//
-//		@Override
-//		public int compare(long[] o1, long[] o2) {
-//			// TODO Auto-generated method stub
-//			return 0;
-//		}
-//		
-//	}
-//	
-//}
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Num1931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] meetings = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer tk = new StringTokenizer(br.readLine());
+            meetings[i][0] = Integer.parseInt(tk.nextToken());
+            meetings[i][1] = Integer.parseInt(tk.nextToken());
+        }
+
+        Arrays.sort(meetings, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return Integer.compare(o1[0], o2[0]);
+                }
+                return Integer.compare(o1[1], o2[1]);
+            }
+        });
+
+        int currentTime = 0;
+        int meeting = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (meetings[i][0] >= currentTime) {
+                meeting++;
+                currentTime = meetings[i][1];
+            }
+        }
+
+        System.out.println(meeting);
+    }
+}
