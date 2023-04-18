507360b5d7468a6a3a309a6424b15368bb0e09f3
Doma-bitcamp <jang6129@naver.com>
2023-04-18 14:57:00 +0900

    Baekjoon 1934 solve

---
+++ src/BaekJoon/Num1934.java
@@ -0,0 +1,30 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Num1934 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer tk = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(tk.nextToken());
+            int b = Integer.parseInt(tk.nextToken());
+
+            sb.append(gcd(a, b) + "\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        else return gcd(b, a % b);
+    }
+
+}
