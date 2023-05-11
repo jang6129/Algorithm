bbafda5f2c2c6d3222752bbfea7fa64c1affa5ad
Doma-bitcamp <jang6129@naver.com>
2023-05-11 18:02:50 +0900

    BaekJoon 1854 unsolve

---
+++ src/BaekJoon/Num1854.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Num1854 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(br.readLine());
+
+        int nodeN = Integer.parseInt(tk.nextToken());
+
+    }
+}
