f583a65ce3a3ce38596bfd31b2eab607bc07b730
Doma-bitcamp <jang6129@naver.com>
2023-06-21 15:27:46 +0900

    Programmers 숫자 문자열과 영단어 solve

--- src/Programmers/숫자_문자열과_영단어.java
+++ src/Programmers/숫자_문자열과_영단어.java
@@ -34,7 +34,7 @@ public class 숫자_문자열과_영단어 {
         String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
         for (int i = 0; i < arr.length; i++) {
-            s.replace(arr[i], Integer.toString(i));
+            s = s.replace(arr[i], Integer.toString(i));
         }
 
         return Integer.parseInt(s);
