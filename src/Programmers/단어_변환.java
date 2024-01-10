061baa0213bcc0a57cf3967458040961a6970713
Doma <jang6129@naver.com>
2024-01-10 22:33:43 +0900

    Programmers 단어 변환 solved

--- src/Programmers/단어_변환.java
+++ src/Programmers/단어_변환.java
@@ -1,4 +1,44 @@
 package Programmers;
 
 public class 단어_변환 {
+    static int answer = Integer.MAX_VALUE;
+    static boolean[] wordUsed;
+
+    public int solution(String begin, String target, String[] words) {
+        wordUsed = new boolean[words.length];
+
+        for (int i = 0; i < words.length; i++) {
+            if (isConvertible(begin, words[i])) {
+                wordUsed[i] = true;
+                dfs(words[i], target, words, 1);
+            }
+        }
+
+        return answer == Integer.MAX_VALUE ? 0 : answer;
+    }
+
+    static void dfs(String begin, String target, String[] words, int count) {
+        if (begin.equals(target)) {
+            answer = Math.min(count, answer);
+            return;
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            if (!wordUsed[i] && isConvertible(begin, words[i])) {
+                wordUsed[i] = true;
+                dfs(words[i], target, words, count + 1);
+                wordUsed[i] = false;
+            }
+        }
+    }
+
+    static boolean isConvertible(String target, String comparison) {
+        int counter = 0;
+        for (int i = 0; i < target.length(); i++) {
+            if (target.charAt(i) != comparison.charAt(i)) counter++;
+        }
+
+        if (counter == 1) return true;
+        else return false;
+    }
 }
