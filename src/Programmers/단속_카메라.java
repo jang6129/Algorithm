b04a56494139b305fab0d92f748decdba5adfe0f
Doma <jang6129@naver.com>
2024-01-26 00:58:32 +0900

    Programmers 단속 카메라 solved

--- src/Programmers/단속_카메라.java
+++ src/Programmers/단속_카메라.java
@@ -1,4 +1,19 @@
 package Programmers;
 
+import java.util.Arrays;
+
 public class 단속_카메라 {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes, (o1, o2) -> o1[1] - o2[1]);
+        int location = routes[0][1];
+        int answer = 1;
+
+        for (int i = 1; i < routes.length; i++) {
+            if (routes[i][0] > location) {
+                answer++;
+                location = routes[i][1];
+            }
+        }
+        return answer;
+    }
 }
