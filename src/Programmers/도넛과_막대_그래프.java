9c94f923806cb41545c15e114fa5c7b7d411377a
Doma <jang6129@naver.com>
2024-01-27 18:36:07 +0900

    Programmers 도넛과 막대 그래프 solving

--- src/Programmers/도넛과_막대_그래프.java
+++ src/Programmers/도넛과_막대_그래프.java
@@ -1,7 +1,43 @@
 package Programmers;
 
-public class 도넛과_막대_그래프 {
-    public void solution() {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    }
+public class 도넛과_막대_그래프 {
+
+    static List<List<Integer>> nodes = new ArrayList<>();
+
+    public int[] solution(int[][] edges) {
+
+        Map<Integer, Integer> pointed = new HashMap<>();
+        int createdNode = 0;
+
+        for (int i = 0; i < edges.length; i++) {
+            nodes.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            int start = edge[0];
+            int destination = edge[1];
+            nodes.get(start).add(destination);
+
+            if (pointed.containsKey(start)) {
+                pointed.put(start, pointed.get(start) + 1);
+            }
+            else pointed.put(start, destination);
+        }
+
+//        createdNode = pointed.
+
+        int[] answer = {};
+        return answer;
+
+    } // solution ends
+
+    public int dfs(int count, int cur, int next, int[][] edges, boolean[] visited) {
+        // if ()
+        return 0;
+    } // dfs ends
 }
