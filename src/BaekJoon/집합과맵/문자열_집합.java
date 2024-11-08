87b27ea70b473b4353f4345061bd5910712a2d44
Doma <jang6129@naver.com>
2024-11-08 17:42:40 +0900

    [BOJ/java] 문자열 집합 solved

---
+++ src/BaekJoon/집합과맵/문자열_집합.java
@@ -0,0 +1,27 @@
+package BaekJoon.집합과맵;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class 문자열_집합 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(sc.next());
+        }
+        int count = 0;
+        for (int i = 0; i < M; i++) {
+            if (set.contains(sc.next())) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
