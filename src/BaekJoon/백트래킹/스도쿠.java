24c0544c54c057c15f8bc54bd29d83ba71471ff4
Doma <jang6129@naver.com>
2024-11-23 17:23:01 +0900

    [BOJ/java] 스도쿠 solving

--- src/BaekJoon/백트래킹/스도쿠.java
+++ src/BaekJoon/백트래킹/스도쿠.java
@@ -3,9 +3,7 @@ package BaekJoon.백트래킹;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class 스도쿠 {
 
